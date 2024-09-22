c4f91c70fa790d304d944c423bba666c523d14ce
mehedi-hasannnn <mhasan2999@gmail.com>
2024-09-22 09:00:41 +0600

    First commit

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// step1: set event handler
+document.getElementById('button-login').addEventListener('click', function(event)
+{
+    // Step2: prevent default behavior
+    event.preventDefault;
+    console.log('Login button clicked');
+
+    // Step3: get the phone number
+    const phoneNumber = document.getElementById('phone-number');
+})
